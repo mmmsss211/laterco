8ed649b6c66c1b0a33f1a9fcd6ba86839b93fdd5
mmmsss211 <mmmsss211@gmail.com>
2022-10-01 18:49:38 +0800

    Added planetscale and prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css'
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material";
+import createEmotionCache from '../utility/createEmotionCache'
+import { CacheProvider } from '@emotion/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) =>{
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Montserrat',
+      ].join(','),
+    },
+  });
+
+  return (
+  <ThemeProvider theme={theme}>
+  <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
